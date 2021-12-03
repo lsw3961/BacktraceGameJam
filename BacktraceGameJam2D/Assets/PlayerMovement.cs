5d289dd32f69c4111a6626a73c8976002143a496
Logan White <lsw3961@g.rit.edu>
2021-12-03 18:23:13 -0500

    Movement Started

---
+++ BacktraceGameJam2D/Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+        //Move method is called
+    }
+    //Movement Method
+    private void Movement() 
+    {
+    
+    }
+}
