ec024a65c2c8eef3f9d8ff6f58733dbb6d5dd55d
mmc9164 <70117876+mmc9164@users.noreply.github.com>
2021-12-04 14:17:46 -0500

    Finished Movement
    Jumping only triggers from ground and cosmetic downward force is applied
    
    Oh I have to add something funny uhhh penis

--- BacktraceGameJam2D/Assets/Scripts/Player/PlayerMovement.cs
+++ BacktraceGameJam2D/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    #region Global Variables
     [SerializeField]
     private float speedX;
 
@@ -11,31 +12,75 @@ public class PlayerMovement : MonoBehaviour
     private float speedY;
 
     [SerializeField]
-    private float gravity;
+    private float cosmeticDownForceValue;
 
     [SerializeField]
     Rigidbody2D rgb;
 
-    private Vector3 xMove;
-    private Vector3 leftMove;
-    private Vector3 upMove;
+    private bool inAir;
+
+    #endregion
+
+    #region Start/Update
+    private void Start()
+    {
+        inAir = true;
+    }
     void Update()
     {
         Movement();
     }
+
+    #endregion
+
+    #region Movement
     //Movement Method
     private void Movement() 
     {
-        #region KeyBinds
+        #region L/R Movement
+
+        //Left + Right Movement
         if (Input.GetAxis("Horizontal") != 0)
         {
             rgb.velocity = new Vector2(speedX * Input.GetAxis("Horizontal"), rgb.velocity.y);
         }
+        #endregion
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        #region Jump Mechanics
+        //Jumping
+        if (Input.GetKeyDown(KeyCode.Space) && !inAir)
         {
-            rgb.AddForce(new Vector2(0, speedY));
+            rgb.AddForce(new Vector2(0, speedY * 100));
+        }
+
+        //Apply Cosmetic Downward Force
+        if(rgb.velocity.y <= 1)
+        {
+            rgb.AddForce(new Vector2(0, (speedY/cosmeticDownForceValue) * (-1)));
         }
         #endregion
     }
+
+    #endregion
+
+    #region Collisions
+    /// <summary>
+    /// Triggers when Player hits object
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        inAir = false;
+    }
+
+    /// <summary>
+    /// Triggers when Player exits Collision event
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        inAir = true;
+    }
+
+    #endregion
 }
