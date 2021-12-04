4204d73c1993c50e45c804c823de3a861231db9f
mmc9164 <70117876+mmc9164@users.noreply.github.com>
2021-12-03 19:57:21 -0500

    Basic Player Movements
    Left and Right are fine, Jumping needs work
    
    Oh right funny comment uhhhh your mom haha goated

--- BacktraceGameJam2D/Assets/Scripts/Player/PlayerMovement.cs
+++ BacktraceGameJam2D/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,20 +4,59 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float speedX;
 
+    [SerializeField]
+    private float speedY;
+
+   // [SerializeField]
+   // private float gravity;
+
+    private Vector3 rightMove;
+    private Vector3 leftMove;
+    private Vector3 upMove;
     void Update()
     {
-        
-        //Move method is called
+        Movement();
     }
     //Movement Method
     private void Movement() 
     {
-    
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            rightMove = new Vector3(speedX, 0, 0);
+        }
+        else
+        {
+            rightMove = new Vector3(0, 0, 0);
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            leftMove = new Vector3(speedX * (-1), 0, 0);
+        }
+        else
+        {
+            leftMove = new Vector3(0, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            upMove = new Vector3(0, speedY , 0);
+        }
+        else
+        {
+            upMove = new Vector3(0, 0, 0);
+        }
+
+        Vector3 movement = new Vector3(rightMove.x + leftMove.x, upMove.y, 0);
+
+        movement *= Time.deltaTime;
+
+        transform.Translate(movement);
+
+        //Debug.Log("Right: " + rightMove.x);
+        //Debug.Log("Left: " + leftMove.x);
     }
 }
