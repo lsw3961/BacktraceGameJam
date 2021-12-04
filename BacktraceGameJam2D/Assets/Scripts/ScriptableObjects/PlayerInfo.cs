96005f3044f84abf22a6b2e6aba5ea57fc39068b
Logan White <lsw3961@g.rit.edu>
2021-12-03 20:00:05 -0500

    Slowly but surely this UI is coming together

---
+++ BacktraceGameJam2D/Assets/Scripts/ScriptableObjects/PlayerInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "PlayerInfo", menuName = "ScriptableObjects/Player", order = 1)]
+public class PlayerInfo : ScriptableObject
+{
+    //list of names for each of the modifiable values
+    public List<string> names;
+    //list of the values for each of the modifiable values
+    public List<string> values;
+}
---
+++ BacktraceGameJam2D/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] PlayerInfo ObjectInformation;
+    [SerializeField] LayerMask notHitableLayers;
+    GameObject currentObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //check what the player has selected as the current object
+        ObjectSelect();
+        if (currentObject != null) {
+            //check if that object has an object information
+            if (currentObject.GetComponent<InfoHolder>())
+            {
+                Debug.Log("has info");
+
+                //instatiate a new prefab for each name we have
+            }
+            else
+                Debug.Log("no Info");
+            //parse the list of values over from the scriptable object
+        }
+
+    }
+    public void ObjectSelect()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+            if (hit.collider != null )
+            {
+                Debug.Log(hit.collider.gameObject);
+                currentObject = hit.collider.gameObject;
+            }
+        }
+    }
+
+    public void CreatePrefabs() 
+    {
+        for (int i = 0;i<ObjectInformation.names.Count;i++) 
+        {
+        
+        }
+    }
+}
