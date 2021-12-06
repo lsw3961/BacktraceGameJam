3c47a62d1a402c20cbfa923e6f5402671831fee7
mmc9164 <70117876+mmc9164@users.noreply.github.com>
2021-12-05 20:29:39 -0500

    Basic Level Select
    No funny comment today :(

---
+++ BacktraceGameJam2D/Assets/Scripts/UI/Level Select/LevelChange.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChange : MonoBehaviour
+{
+    public int levelIndex;
+    public void LevelSwitch()
+    {
+        SceneManager.LoadSceneAsync(levelIndex);
+    }
+}
---
+++ BacktraceGameJam2D/Assets/Scripts/UI/Level Select/LevelImageClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelImageClick : MonoBehaviour
+{
+    
+    public void ImageClickRecieve(int associatedIndex)
+    {
+        GameObject startButton = GameObject.Find("StartButton");
+        startButton.GetComponent<LevelChange>().levelIndex = associatedIndex;
+    }
+}
