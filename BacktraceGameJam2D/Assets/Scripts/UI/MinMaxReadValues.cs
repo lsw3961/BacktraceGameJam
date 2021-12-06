ffb38965e1cafa96fbdd3b0e2e4783d56dd32488
Logan White <lsw3961@g.rit.edu>
2021-12-05 19:37:03 -0500

    UI is looking Better

---
+++ BacktraceGameJam2D/Assets/Scripts/UI/MinMaxReadValues.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class MinMaxReadValues : MonoBehaviour
+{
+    Slider slider;
+    [SerializeField] TMP_Text minValue;
+    [SerializeField]TMP_Text maxValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        if (minValue.text != slider.minValue.ToString())
+        {
+            minValue.text = slider.minValue.ToString();
+        }
+        if (maxValue.text != slider.maxValue.ToString())
+        {
+            maxValue.text = slider.maxValue.ToString();
+        }
+    }
+
+}
