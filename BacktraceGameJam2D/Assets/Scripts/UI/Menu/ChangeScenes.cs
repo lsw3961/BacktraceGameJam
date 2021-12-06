3fafd3d1c5d9dc9f0e223534199fcc2f82aa03f4
Logan White <lsw3961@g.rit.edu>
2021-12-05 21:50:30 -0500

    Narrative is done and everything is fitted together

--- BacktraceGameJam2D/Assets/Scripts/UI/Menu/ChangeScenes.cs
+++ BacktraceGameJam2D/Assets/Scripts/UI/Menu/ChangeScenes.cs
@@ -8,4 +8,9 @@ public class ChangeScenes : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void MovetoScene(int sceneNum) 
+    {
+        SceneManager.LoadScene(sceneNum);
+
+    }
 }
