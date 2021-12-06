3ec29f43ccfb73625a7ddec654f773a9f862b1cc
Logan White <lsw3961@g.rit.edu>
2021-12-05 22:51:13 -0500

    added the readme and an anim for the final scene and typewriter text for the narrative scenes

---
+++ BacktraceGameJam2D/Assets/README.txt
@@ -0,0 +1,12 @@
+Hello,
+We want to thank all of the people that let us user their assets:
+
+https://we.graphics/blog/create-a-detailed-vintage-tv-from-scratch-in-photoshop/
+
+they gave us the filmic mask of the TV that we used
+
+https://modthesims.info/d/344317/quot-the-matrix-quot-pattern.html
+
+this website gave us the scrolling background
+
+thank you to backtrace for funding this competition and we hope you have a great time playing it
---
+++ BacktraceGameJam2D/Assets/Scripts/UI/TypeWriterText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TypeWriterText : MonoBehaviour
+{
+    // Start is called before the first frame update
+   TMP_Text text;
+    string story;
+    [SerializeField] float SecondsPerLetter = 0.125f;
+    private void Awake()
+    {
+        text = GetComponent<TMP_Text>();
+        story = text.text;
+        text.text = "";
+
+        StartCoroutine("PlayText");
+    }
+
+    IEnumerator PlayText() 
+    {
+        foreach (char c in story) 
+        {
+            text.text += c;
+            yield return new WaitForSeconds(SecondsPerLetter);
+        }
+    }
+}
